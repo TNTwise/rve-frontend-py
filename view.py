272eec14f8127a97f9e5ae78c2acc544f458a26d
tntwise <tntwise3@gmail.com>
2024-07-14 17:02:45 -0500

    add bottom bar

--- view.py
+++ view.py
@@ -1,19 +1,19 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from mainwindow import Ui_MainWindow # Import the UI class from the converted module
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.uic import loadUiType
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        # Set up the user interface from Designer.
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.ui.homeBtn.clicked.connect(lambda: self.ui.homeBtn.setStyleSheet('QPushButton:checked { background-color: #6FC; }'
-                           'QPushButton { background-color: #CCC; }'))
+# Load the .ui file
+def load_ui(ui_file, base_instance=None):
+   
+    ui = loadUiType(ui_file, base_instance)
+    return ui
 
 if __name__ == "__main__":
+    import sys
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    
+    # Example usage
+    ui_file = "testRVEInterface.ui"
+    widget = load_ui(ui_file)
+    widget.show()
+    
     sys.exit(app.exec_())
