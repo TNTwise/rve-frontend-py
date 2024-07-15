9ced63ba3bac3146af1d9de13e009ef739b0372e
tntwise <tntwise3@gmail.com>
2024-07-15 04:01:48 +0000

    build resources file

--- build.py
+++ build.py
@@ -1,9 +1,13 @@
 from PyQt6 import sip
 from PyQt6.uic import compileUi
-
+import os
 def build_gui():
     with open("mainwindow.py", 'w') as f:
         compileUi("testRVEInterface.ui",f)
         f.write('\nimport resources_rc')
+def build_resources():
+    os.system("pyside6-rcc -o resources_rc.py resources.qrc")
+
 
 build_gui()
+build_resources()
